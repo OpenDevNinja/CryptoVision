c5f8df610bb015f71da82859f900949ccfef9a8a
Bonaventure <toybpro@gmail.com>
2025-04-27 17:33:59 +0100

    first commit2

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './contexts/ThemeContext';
-import { Home } from './pages/Home';
-import { Crypto } from './pages/Crypto';
-import { News } from './pages/News';
-import { About } from './pages/About';
-import { Contact } from './pages/Contact';
-import { NotFound } from './pages/NotFound';
-import { Header } from './components/common/Header';
-import { Footer } from './components/common/Footer';
+import React from 'react';
 
-
-
-function App() {
+const App = () => {
   return (
-    <ThemeProvider>
-      <Router>
-        <div className="min-h-screen flex flex-col">
-          <Header />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/crypto" element={<Crypto />} />
-              <Route path="/news" element={<News />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </ThemeProvider>
+    <div>
+       <h1> B L O C K C H I N  </h1>
+    </div>
   );
 }
 
