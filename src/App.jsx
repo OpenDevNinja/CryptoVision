afd000e0c131fa3ce107f5884a53d4d0395026af
Bonaventure <toybpro@gmail.com>
2025-04-27 15:14:04 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import { NotFound } from './pages/NotFound';
 import { Header } from './components/common/Header';
 import { Footer } from './components/common/Footer';
 
+
+
 function App() {
   return (
     <ThemeProvider>
